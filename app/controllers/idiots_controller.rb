fa50ab4bfb59e295ffdffc7c92efe124ab76d8ca
Mikhail Sotnikov <thehofch@gmail.com>
2020-03-01 01:36:01 +0300

    finish controller

--- app/controllers/idiots_controller.rb
+++ app/controllers/idiots_controller.rb
@@ -1,18 +1,44 @@
 class IdiotsController < ApplicationController
+  before_action :load_record, only: %i[show update destroy]
+
   def index
     idiots = Idiot.all
     render json: idiots, each_serializer: IdiotSerializer
   end
 
   def show
+    render json: @idiot, serializer: IdiotSerializer
   end
 
   def create
+    idiot = Idiot.new(permitted_attributes)
+    if idiot.save
+      render json: idiot, serializer: IdiotSerializer, status: :created
+    else
+      render json: idiot.errors.messages, status: :bad_request
+    end
   end
 
   def update
+    if @idiot.update(permitted_attributes)
+      render json: @idiot, serializer: IdiotSerializer
+    else
+      render json: @idiot.errors.messages, status: :bad_request
+    end
   end
 
   def destroy
+    @idiot.destroy
+    head :no_content
+  end
+
+  private
+
+  def load_record
+    @idiot = Idiot.find(params[:id])
+  end
+
+  def permitted_attributes
+    params.require(:idiot).permit(:name, :death_date, :r_star_id, :pazient_diagnos, :comment, :agressive, :mom_joke)
   end
 end
